934546bd39a331648b1e0ec8865748fc4dd113b9
Michael Wohner <68833089+v11stats@users.noreply.github.com>
2024-11-09 05:45:30 -0800

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import ClimbingTracker from './components/ClimbingTracker';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <ClimbingTracker />
+    </main>
+  );
+}
