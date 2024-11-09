6bf81755f61c992a0180d02bc163ca486eea5783
Michael Wohner <68833089+v11stats@users.noreply.github.com>
2024-11-09 07:24:46 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Ensure all components in src are scanned
     './src/app/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/components/ui/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
